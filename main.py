5354d5b54b043045c3b438eb0f82b3ed2f9ba204
GitKodland <125306926+GitKodland@users.noreply.github.com>
2025-10-15 11:49:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-# Import
-from flask import Flask, render_template,request, redirect
+# Importing
+from flask import Flask, render_template, request, redirect, session
 # Connecting the database library
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-# Connecting SQLite
+# Setting the secret key for the session
+app.secret_key = 'my_top_secret_123'
+# Establishing SQLite connection
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Creating a DB
@@ -13,57 +15,50 @@ db = SQLAlchemy(app)
 # Creating a table
 
 class Card(db.Model):
-    # Creating columns
+    # Establishing entry fields
     # id
     id = db.Column(db.Integer, primary_key=True)
     # Title
     title = db.Column(db.String(100), nullable=False)
-    # Description
+    # Subtitle
     subtitle = db.Column(db.String(300), nullable=False)
     # Text
     text = db.Column(db.Text, nullable=False)
+    # The card owner's email
+    user_email = db.Column(db.String(100), nullable=False)
 
-    # Outputting the object and id
+    # Outputting object and its ID
     def __repr__(self):
         return f'<Card {self.id}>'
     
 
-#Assignment #2. Create the User table
+# Assignment #1. Create User table
 
 
-
-
-
-
-
-
-
-# Running the content page
+# Launching content page
 @app.route('/', methods=['GET','POST'])
 def login():
-        error = ''
-        if request.method == 'POST':
-            form_login = request.form['email']
-            form_password = request.form['password']
+    error = ''
+    if request.method == 'POST':
+        form_login = request.form['email']
+        form_password = request.form['password']
             
-            #Assignment #4. Implement authorisation
-            
+        # Assignment #4. Implement user verification
 
-
-            
-        else:
-            return render_template('login.html')
+     
+    else:
+        return render_template('login.html')
 
 
 
 @app.route('/reg', methods=['GET','POST'])
 def reg():
     if request.method == 'POST':
-        login= request.form['email']
+        email = request.form['email']
         password = request.form['password']
         
-        #Assignment #3. Make it so that the user's data is recorded to the database
-        
+        # Assignment #3. Implement user recording
+
 
         
         return redirect('/')
@@ -72,26 +67,26 @@ def reg():
         return render_template('registration.html')
 
 
-# Running the content page
+# Launching content page
 @app.route('/index')
 def index():
-    # Displaying the databaase entries
+    # Assignment #4. Make sure user only sees their own cards
     cards = Card.query.order_by(Card.id).all()
     return render_template('index.html', cards=cards)
 
-# Running the page with the entry
+# Launching the card page
 @app.route('/card/<int:id>')
 def card(id):
     card = Card.query.get(id)
 
     return render_template('card.html', card=card)
 
-# Running the entry creation page
+# Launching the card creation page
 @app.route('/create')
 def create():
     return render_template('create_card.html')
 
-# The entry forn
+# The card form
 @app.route('/form_create', methods=['GET','POST'])
 def form_create():
     if request.method == 'POST':
@@ -99,7 +94,7 @@ def form_create():
         subtitle =  request.form['subtitle']
         text =  request.form['text']
 
-        # Creating an object that will be sent to the DB
+        # Assignment #4. Make card creation happen on behalf of the user
         card = Card(title=title, subtitle=subtitle, text=text)
 
         db.session.add(card)
@@ -107,10 +102,6 @@ def form_create():
         return redirect('/index')
     else:
         return render_template('create_card.html')
-
-
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
